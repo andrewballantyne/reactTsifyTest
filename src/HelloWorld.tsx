12d1ecbe1e052c1a5f856f3e542db8aa58dbbb16
Andrew Ballantyne <ballantyno@gmail.com>
2016-08-20 21:33:56 -0400

    Adds more structure and extra hints.

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,19 +1,45 @@
+/** We of course need our React external library **/
 import * as React from 'react';
 
+/**
+ * Props/State defining...
+ *
+ * One of many ways to create props and state; React.Component requires both <Props, State>.
+ *
+ * Other ways can include:
+ *   // Directly
+ *   React.Component<{...}, {...}>
+ *
+ *   // A 'don't care' approach; 'any' type is of course any structure
+ *   React.Component<any, any>
+ */
 type props = {
-  name?:string // note the ?, this denotes the typescript optional parameter
+  /** Standard property definitions for any TypeScript 'type'; the ? denotes optional */
+  name?:string
 };
 type state = {};
 
 class HelloWorld extends React.Component<props, state> {
-  render():JSX.Element {
+  public render():JSX.Element {
     const { name } = this.props;
     return (
       <div>
-        The world says 'Hello', {name || 'Batman'}.
+        <h1 style={this.getStyles()}>The world says 'Hello', {name || 'Batman'}.</h1>
+        <p>Open the code and look in the src folder for main.tsx and HelloWorld.tsx for inline
+          comments.</p>
       </div>
     );
   }
+
+  private getStyles():Object {
+    return {
+      color: '#999'
+    };
+  }
 }
 
+/** Expose our props; possible use of someone wanting to reference it as part of their props **/
+export type HelloWorldProps = props;
+
+/** Allows this file to be a target of 'import HelloWorld from 'HelloWorld';' **/
 export default HelloWorld;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
-/// <reference path="_typings/react-all.d.ts" />
+/** We include all our TypeScript typings (definitions for the TypeScript compiler) here **/
+/// <reference path="_typings/include.d.ts" />
+
+/** Then we import the needed external libraries **/
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+/** Of course we need our root component... **/
 import HelloWorld from './HelloWorld';
+/** BONUS! If you wanted to reference the props; the below line could replace the above line **/
+// import HelloWorld, { HelloWorldProps } from './HelloWorld';
 
-// ReactDOM.render(<HelloWorld />, document.getElementById('root'));
-ReactDOM.render(<HelloWorld name="Bruce Wayne" />, document.getElementById('root'));
+/** Render **/
+ReactDOM.render(
+  /** Two ways you can call the react component **/
+  <HelloWorld name="Bruce Wayne" />,
+  // <HelloWorld />,
+  /** Our root div from the out/index.html **/
+  document.getElementById('root')
+);
